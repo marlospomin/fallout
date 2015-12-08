fc1836061ef8fc2a7525a34dcfcd74f698a8e6cf
Marlos Pomin <marlospomin@gmail.com>
2015-12-08 16:39:00 -0200

    Add remaining files

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(function () {
+  
+  "use strict";
+  $(".owl-carousel").owlCarousel({
+    autoPlay: true,
+    singleItem: true
+/*  navigation: true,
+    navigationText: [
+      "<i class='fa fa-arrow-left'></i>",
+      "<i class='fa fa-arrow-right'></i>"
+    ] */
+  });
+  
+  var $container = $(".grid").isotope({
+    
+    itemSelector: '.grid-item',
+    percentPosition: true
+  });
+  
+  $(".filter").on('click', 'button', function () {
+    
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+  
+  $(".btn-group").each(function (i, buttonGroup) {
+    
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function () {
+      $buttonGroup.find('.active').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
+  
+  function animatedHeader() {
+    window.addEventListener('scroll', function (e) {
+      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 550;
+      if (distanceY > shrinkOn) {
+        $("header").addClass('shrink');
+      } else {
+        $("header").removeClass('shrink');
+      }
+    });
+  }
+  
+  animatedHeader();
+  
+});
